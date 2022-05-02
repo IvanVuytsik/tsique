47999a4e565fe67deacf3e70c25221cf5754ac2e
Ivan Vuytsik <vuicik1989@gmail.com>
2022-05-02 18:36:54 +0300

    commit

--- src/components/scene/Scene.jsx
+++ src/components/scene/Scene.jsx
@@ -1,20 +1,43 @@
-import React, { Suspense } from "react";
-import { Canvas } from "@react-three/fiber";
-import { OrbitControls } from "@react-three/drei";
+import React, { Suspense, useRef } from "react";
+import { Canvas} from "@react-three/fiber";
+import { OrbitControls, useHelper  } from "@react-three/drei";
+import { SpotLightHelper, PointLightHelper } from "three"
 
 import Codebear from "./Codebear.js";
 // import "./scene.css";
 
 function Scene() {
-  return (
-    <Canvas orthographic camera={{ zoom: 90, position: [360, 100, 100] }}>
 
-      <OrbitControls />
-      <ambientLight intensity={0.6} />
-      {/* // <pointLight position={[10, 10, 10]} /> 
-      // // <directionalLight intensity={0.5} /> */}
+   
+  return (
+    <Canvas 
+       
+      camera={{ position: [-5, 4, -10], fov: 35 }}>
+
+      <ambientLight intensity={0.3} />
+
+      {/* <directionalLight
+          castShadow
+          position={[0, 10, 0]}
+          intensity={1.5}
+          shadow-mapSize-width={1024}
+          shadow-mapSize-height={1024}
+          shadow-camera-far={50}
+          shadow-camera-left={-10}
+          shadow-camera-right={10}
+          shadow-camera-top={10}
+          shadow-camera-bottom={-10}
+        /> */}
+
+      <spotLight position={[-10, 5, 0]} intensity={2.5} color="#e19b00" />
+      <spotLight position={[15, 10, 0]} intensity={2.5} color="#dc9800" />
+      {/* <spotLight position={[0, 10, -10]} intensity={2.5} color="#3006b1" /> */}
+      
+      
+
       <Codebear />
-    
+      <OrbitControls />
+
     </Canvas>
   )
 }
