b108b10b528191daec7eea74d84e01244639be72
Ivan Vuytsik <vuicik1989@gmail.com>
2022-05-28 23:35:38 +0300

    commit

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
 import './footer.css'
 
-import {FaLinkedin} from 'react-icons/fa'
-import {FaGithub} from 'react-icons/fa'
-import {GrYoutube} from 'react-icons/gr'
-import {IoLogoGameControllerA} from 'react-icons/io'
+import { FaLinkedin, FaItchIo, FaGithub } from 'react-icons/fa'
+import { GrYoutube } from 'react-icons/gr'
+ 
 
 const Footer = () => {
   return (
@@ -25,7 +24,7 @@ const Footer = () => {
         <a href="https://www.linkedin.com/in/ivan-vuytsik-82708471/" target="_blank"><FaLinkedin/></a>
         <a href="https://github.com/IvanVuytsik" target="_blank"><FaGithub/></a>
         <a href="https://www.youtube.com/channel/UCwNV-AEok2W6LEan95xZicQ" target="_blank"><GrYoutube/></a>
-        <a href="https://powerhamster.itch.io/" target="_blank"><IoLogoGameControllerA/></a>
+        <a href="https://powerhamster.itch.io/" target="_blank"><FaItchIo/></a>
       </div>
 
       <div className="footer__copyright">
--- src/components/header/Socials.jsx
+++ src/components/header/Socials.jsx
@@ -1,17 +1,16 @@
 import React from 'react'
-import {FaLinkedin} from 'react-icons/fa'
-import {FaGithub} from 'react-icons/fa'
-import {GrYoutube} from 'react-icons/gr'
-import {IoLogoGameControllerA} from 'react-icons/io'
-
+import { FaLinkedin, FaItchIo, FaGithub } from 'react-icons/fa'
+import { GrYoutube } from 'react-icons/gr'
  
+
+
 const HeaderSocials = () => {
   return (
     <div className='header__socials'>
         <a href="https://www.linkedin.com/in/ivan-vuytsik-82708471/" target="_blank"><FaLinkedin/></a>
         <a href="https://github.com/IvanVuytsik" target="_blank"><FaGithub/></a>
         <a href="https://www.youtube.com/channel/UCwNV-AEok2W6LEan95xZicQ" target="_blank"><GrYoutube/></a>
-        <a href="https://powerhamster.itch.io/" target="_blank"><IoLogoGameControllerA/></a>
+        <a href="https://powerhamster.itch.io/" target="_blank"><FaItchIo/></a>
 
     </div>
   )
