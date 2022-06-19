66e0482ff4f6721a4609edba16718599f5b592af
Ivan Vuytsik <vuicik1989@gmail.com>
2022-06-19 10:14:29 +0300

    commit

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import './portfolio.css'
 import IMG0 from '../../assets/project_0.png'
 import IMG1 from '../../assets/project_1.png'
+import IMG2 from '../../assets/project_2.png'
+import { Hidden } from '@material-ui/core'
 const data = [
 {
   id: 0,
@@ -9,6 +11,7 @@ const data = [
   title: 'Python Game Blueprints',
   github: 'https://github.com/IvanVuytsik/python-rpg-box',
   demo: 'https://www.youtube.com/channel/UCwNV-AEok2W6LEan95xZicQ/videos',
+  status: 'flex',
 },
 {
   id: 1,
@@ -16,6 +19,15 @@ const data = [
   title: 'Three.js Earth',
   github: 'https://github.com/IvanVuytsik/earth_element',
   demo: 'https://earthelement.vercel.app/',
+  status: 'flex',
+},
+{
+  id: 2,
+  image: IMG2,
+  title: 'Spellpot Studio',
+  github: '',
+  demo: 'https://spellpot.com/',
+  status: 'none',
 },
 ]
 
@@ -28,7 +40,7 @@ const Portfolio = () => {
 
       <div className="container portfolio__container">
         {
-          data.map(({id, image, title, github, demo}) => {
+          data.map(({id, image, title, github, demo, status}) => {
             return ( 
               <article key={id} className='portfolio__item'>
                 <div className='portfolio__item-image'>
@@ -36,7 +48,7 @@ const Portfolio = () => {
                 </div>
                 <h3>{title}</h3>
                 <div className='portfolio__item-cta'>
-                  <a href={github} className='btn' target='_blank'>Github</a>
+                  <a href={github} className='btn' target='_blank' style={{display: `${status}`}}>Github</a>
                   <a href={demo} className='btn btn-primary' target='_blank'>Live Demo</a>
                 </div>
               </article>
